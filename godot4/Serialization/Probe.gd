29aaf6ea7a3540f3ca178a8eedcf3af38b881424
hilfazer <az13337@gmail.com>
2023-06-22 17:20:03 +0200

    style guide

--- godot4/Serialization/Probe.gd
+++ godot4/Serialization/Probe.gd
@@ -12,34 +12,34 @@ class Probe extends RefCounted:
 	# creation of other nodes needs to be taken care of outside of
 	# deserialize( node ) (i.e. _init(), _ready())
 	# or deserialize( node ) won't deserialize them nor their branch
-	var nodesNotInstantiable : Array[Node]
-	var nodesNoMatchingDeserialize : Array[Node]
+	var nodes_not_instantiable : Array[Node]
+	var nodes_no_matching_deserialize : Array[Node]
 
 
 	func _init( node : Node ):
 		if node.owner == null and node.filename.is_empty():
-			_addNotInstantiable( node )
+			_add_not_instantiable( node )
 
 		if node.has_method(SerializerGd.SERIALIZE) \
 				and not node.has_method(SerializerGd.DESERIALIZE):
-			_addNoMatchingDeserialize( node )
+			_add_no_matching_deserialize( node )
 
 		for child in node.get_children():
 			_merge( Probe.new( child ) )
 
 
 	func _merge( other : Probe ):
-		for i in other.nodesNotInstantiable:
-			nodesNotInstantiable.append( i )
-		for i in other.nodesNoMatchingDeserialize:
-			nodesNoMatchingDeserialize.append( i )
+		for i in other.nodes_not_instantiable:
+			nodes_not_instantiable.append( i )
+		for i in other.nodes_no_matching_deserialize:
+			nodes_no_matching_deserialize.append( i )
 
 
-	func _addNotInstantiable( node : Node ):
-		if nodesNotInstantiable.find( node ) == -1:
-			nodesNotInstantiable.append( node )
+	func _add_not_instantiable( node : Node ):
+		if nodes_not_instantiable.find( node ) == -1:
+			nodes_not_instantiable.append( node )
 
 
-	func _addNoMatchingDeserialize( node : Node ):
-		if nodesNoMatchingDeserialize.find( node ) == -1:
-			nodesNoMatchingDeserialize.append( node )
+	func _add_no_matching_deserialize( node : Node ):
+		if nodes_no_matching_deserialize.find( node ) == -1:
+			nodes_no_matching_deserialize.append( node )
--- godot4/Serialization/tests/unit/Test_Probe.gd
+++ godot4/Serialization/tests/unit/Test_Probe.gd
@@ -2,14 +2,14 @@ extends "res://tests/gut_test_base.gd"
 
 const Scene1Scn              = preload("res://tests/files/Scene1.tscn")
 const SceneNoDeserializeGd   = preload("res://tests/files/NoDeserialize.tscn")
-const ProbeGd                = preload("res://Probe.gd")
+const ProbeGd                = preload("res://probe.gd")
 
 
 func test_probeValidSubtree():
 	var scene1 = autofree( Scene1Scn.instantiate() )
 	var probe = ProbeGd.scan( scene1 )
-	assert_eq( probe.nodesNotInstantiable.size(), 0 )
-	assert_eq( probe.nodesNoMatchingDeserialize.size(), 0 )
+	assert_eq( probe.nodes_not_instantiable.size(), 0 )
+	assert_eq( probe.nodes_no_matching_deserialize.size(), 0 )
 
 
 func test_noninstantiableSubtree():
@@ -18,21 +18,21 @@ func test_noninstantiableSubtree():
 	node.name = "CantInstance"
 	scene1.add_child( node )
 	var probe = ProbeGd.scan( scene1 )
-	assert_eq( probe.nodesNotInstantiable.size(), 1 )
-	assert_eq( probe.nodesNoMatchingDeserialize.size(), 0 )
+	assert_eq( probe.nodes_not_instantiable.size(), 1 )
+	assert_eq( probe.nodes_no_matching_deserialize.size(), 0 )
 
-	if probe.nodesNotInstantiable.size() == 1:
-		assert_eq( probe.nodesNotInstantiable[0].name, "CantInstance" )
+	if probe.nodes_not_instantiable.size() == 1:
+		assert_eq( probe.nodes_not_instantiable[0].name, "CantInstance" )
 
 
 func test_nonserializableSubtree():
 	var scene1 = autofree( SceneNoDeserializeGd.instantiate() )
 	var probe = ProbeGd.scan( scene1 )
-	assert_eq( probe.nodesNotInstantiable.size(), 0 )
-	assert_eq( probe.nodesNoMatchingDeserialize.size(), 1 )
+	assert_eq( probe.nodes_not_instantiable.size(), 0 )
+	assert_eq( probe.nodes_no_matching_deserialize.size(), 1 )
 
-	if probe.nodesNoMatchingDeserialize.size() == 1:
-		assert_eq( probe.nodesNoMatchingDeserialize[0].name, "NoDeserialize" )
+	if probe.nodes_no_matching_deserialize.size() == 1:
+		assert_eq( probe.nodes_no_matching_deserialize[0].name, "NoDeserialize" )
 
 
 func test_NonInstantiableOutsideTree():
