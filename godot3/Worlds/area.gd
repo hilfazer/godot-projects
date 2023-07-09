46855f1a52c92e5d20bdcddfc7c951a97af0de9d
hilfazer <az13337@gmail.com>
2023-07-09 13:01:03 +0200

    drawing circles on collision shapes

--- godot3/Worlds/area.gd
+++ godot3/Worlds/area.gd
@@ -1,7 +1,11 @@
 extends Area2D
 
+export var _circle_color: Color
+
 var _collide_color: = Color(Color.green)
 var _no_collide_color: = Color(Color.whitesmoke)
+
+onready var circle_shape: CircleShape2D = $'CollisionShape2D'.shape
 
 
 func _physics_process(_delta):
@@ -9,3 +13,7 @@ func _physics_process(_delta):
 		$'ColorRect'.color = _no_collide_color
 	else:
 		$'ColorRect'.color = _collide_color
+
+
+func _draw():
+	draw_circle($'CollisionShape2D'.position, circle_shape.radius, _circle_color)
