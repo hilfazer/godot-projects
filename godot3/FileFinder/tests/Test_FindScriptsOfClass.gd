d07f5e6731daef17b87df2bb1ad2264b77ac485c
hilfazer <az13337@gmail.com>
2023-07-12 21:21:08 +0200

    testing FileFinderGd.findScriptsOfClass(scripts, NodeSubclassGd)

--- godot3/FileFinder/tests/Test_FindScriptsOfClass.gd
+++ godot3/FileFinder/tests/Test_FindScriptsOfClass.gd
@@ -1,6 +1,7 @@
 extends "res://tests/GutTestBase.gd"
 
 const FileFinderGd = preload("res://FileFinder.gd")
+const NodeSubclassGd = preload("res://tests/files/NodeSubclass.gd")
 
 
 func test_findNodeSubclasses():
@@ -11,3 +12,6 @@ func test_findNodeSubclasses():
 
 	var node2Dsubclasses = FileFinderGd.findScriptsOfClass(scripts, Node2D)
 	assert_eq(node2Dsubclasses.size(), 0)
+
+	var node_subclass_subclasses = FileFinderGd.findScriptsOfClass(scripts, NodeSubclassGd)
+	assert_eq(node_subclass_subclasses.size(), 1)
