8cbe3cd2884b1898a183dcac053bd252be0ee703
hilfazer <az13337@gmail.com>
2023-07-15 16:27:16 +0200

    node subsubclass

---
+++ godot4/MemoryConsumption/node_subsubclass_line.gd
@@ -0,0 +1,26 @@
+@tool
+extends "res://abstract_type_line.gd"
+
+
+var objects := []
+
+
+func _create( count : int ) -> int:
+	objects.resize(count)
+	for i in objects.size():
+		objects[i] = MyMyNode.new()
+	return OK
+
+
+func _destroy():
+	for i in objects.size():
+		objects[i].free()
+	objects.clear()
+
+
+class MyNode extends Node:
+	pass
+
+
+class MyMyNode extends MyNode:
+	pass
