f0da599502213ad209a7488953a23d798d60c0aa
hilfazer <az13337@gmail.com>
2023-07-29 16:38:21 +0200

    UnitGrabbingAgent.gd movement update

--- godot4/DungeonCrawler/engine/agent/UnitGrabbingAgent.gd
+++ godot4/DungeonCrawler/engine/agent/UnitGrabbingAgent.gd
@@ -1,8 +1,7 @@
 extends AgentBase
 
 
-var _pressedDirections :PackedByteArray = [0, 0, 0, 0]
-
+var _direction := Vector2.ZERO
 
 func _ready():
 	var parent = get_parent()
@@ -11,35 +10,23 @@ func _ready():
 
 
 func _physics_process( _delta ):
-	var direction := Vector2(
-		_pressedDirections[3] - _pressedDirections[2], _pressedDirections[1] - _pressedDirections[0]
-		)
-	if !direction:
-		return
-
-	for unit in _unitsInTree:
-		unit.requestedDirection = direction
+	if _direction != Vector2.ZERO:
+		for unit in _unitsInTree:
+			unit.requestedDirection = Vector2i(_direction)
 
 
 func _unhandled_input(event):
-	if event.is_action_pressed("move_up"):
-		_pressedDirections[0] = 1
-	elif event.is_action_pressed("move_down"):
-		_pressedDirections[1] = 1
-	elif event.is_action_pressed("move_left"):
-		_pressedDirections[2] = 1
-	elif event.is_action_pressed("move_right"):
-		_pressedDirections[3] = 1
-	elif event.is_action_released("move_up"):
-		_pressedDirections[0] = 0
-	elif event.is_action_released("move_down"):
-		_pressedDirections[1] = 0
-	elif event.is_action_released("move_left"):
-		_pressedDirections[2] = 0
-	elif event.is_action_released("move_right"):
-		_pressedDirections[3] = 0
+	if 		event.is_action_pressed("move_up") or \
+			event.is_action_pressed("move_down") or \
+			event.is_action_pressed("move_left") or \
+			event.is_action_pressed("move_right") or \
+			event.is_action_released("move_up") or \
+			event.is_action_released("move_down") or \
+			event.is_action_released("move_left") or \
+			event.is_action_released("move_right"):
+		_direction = \
+			Input.get_vector('move_left', 'move_right', 'move_up', 'move_down').snapped(Vector2.ONE)
 	else:
 		return
 
-	get_viewport().set_input_as_handled()
-
+	get_viewport().set_input_as_handled() 
