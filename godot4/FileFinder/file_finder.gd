a3f1dc2f3c4c7c8165c2603089150e6b05246596
hilfazer <az13337@gmail.com>
2023-08-04 18:12:05 +0200

    replaced list_dir_begin()/list_dir_end() with get_files() and get_directories()

--- godot4/FileFinder/file_finder.gd
+++ godot4/FileFinder/file_finder.gd
@@ -8,30 +8,23 @@ static func find_files_in_directory( directoryPath: String, extensionFilter := "
 	assert( directoryPath )
 	assert( extensionFilter == "" or extensionFilter.get_extension() != "" )
 
-	var dir: DirAccess = DirAccess.open(directoryPath)
+	var dir :DirAccess = DirAccess.open(directoryPath)
 	if dir == null:
 		return PackedStringArray()
 
-	var filePaths := PackedStringArray()
-	dir.list_dir_begin() # TODOGODOT4 fill missing arguments https://github.com/godotengine/godot/pull/40547
+	var files_to_return := PackedStringArray()
 
-	var file : String = dir.get_next()
-	while file != "":
-		if dir.current_is_dir():
-			var subdirFilePaths := find_files_in_directory( \
-					dir.get_current_dir().path_join( file ), extensionFilter )
-			filePaths.append_array( subdirFilePaths )
+	for file in dir.get_files():
+		assert( dir.file_exists( file ) )
+		if !extensionFilter or  "." + file.get_extension() == extensionFilter:
+			files_to_return.append( dir.get_current_dir().path_join( file ) )
+	
+	for subdir in dir.get_directories():
+		var subdirFilePaths := find_files_in_directory( \
+				dir.get_current_dir().path_join( subdir ), extensionFilter )
+		files_to_return.append_array( subdirFilePaths )
 
-		else:
-			assert( dir.file_exists( file ) )
-			if !extensionFilter or  "." + file.get_extension() == extensionFilter:
-				filePaths.append( dir.get_current_dir().path_join( file ) )
-
-		file = dir.get_next()
-
-	dir.list_dir_end()
-
-	return filePaths
+	return files_to_return
 
 
 #this includes subclasses
