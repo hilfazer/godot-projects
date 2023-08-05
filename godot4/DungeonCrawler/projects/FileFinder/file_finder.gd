8e98ee06c107e24352d878e7e80e7397c6ee707d
hilfazer <az13337@gmail.com>
2023-08-05 15:27:56 +0200

    FileFinder update

--- godot4/DungeonCrawler/projects/FileFinder/file_finder.gd
+++ godot4/DungeonCrawler/projects/FileFinder/file_finder.gd
@@ -1,42 +1,38 @@
-extends RefCounted
+extends Object
+
 
 func _init():
 	assert(false)
 
 
-static func find_files_in_directory( directoryPath: String, extensionFilter := "" ) -> PackedStringArray:
-	assert( directoryPath )
-	assert( extensionFilter == "" or extensionFilter.get_extension() != "" )
+static func find_files_in_directory(
+		directory :String, extension_filter := "" ) -> PackedStringArray:
 
-	var dir: DirAccess = DirAccess.open(directoryPath)
+	assert( directory )
+	assert( extension_filter == "" or extension_filter.get_extension() != "" )
+
+	var dir :DirAccess = DirAccess.open(directory)
 	if dir == null:
 		return PackedStringArray()
 
-	var filePaths := PackedStringArray()
-	dir.list_dir_begin() # TODOGODOT4 fill missing arguments https://github.com/godotengine/godot/pull/40547
+	var files_to_return := PackedStringArray()
 
-	var file : String = dir.get_next()
-	while file != "":
-		if dir.current_is_dir():
-			var subdirFilePaths := find_files_in_directory( \
-					dir.get_current_dir().path_join( file ), extensionFilter )
-			filePaths.append_array( subdirFilePaths )
+	for file in dir.get_files():
+		assert( dir.file_exists( file ) )
+		if !extension_filter or  "." + file.get_extension() == extension_filter:
+			files_to_return.append( dir.get_current_dir().path_join( file ) )
+	
+	for subdir in dir.get_directories():
+		var subdirFilePaths := find_files_in_directory( \
+				dir.get_current_dir().path_join( subdir ), extension_filter )
+		files_to_return.append_array( subdirFilePaths )
 
-		else:
-			assert( dir.file_exists( file ) )
-			if !extensionFilter or  "." + file.get_extension() == extensionFilter:
-				filePaths.append( dir.get_current_dir().path_join( file ) )
-
-		file = dir.get_next()
-
-	dir.list_dir_end()
-
-	return filePaths
+	return files_to_return
 
 
 #this includes subclasses
-static func find_scripts_of_class( scripts: PackedStringArray, klass ):
-	var scriptsToReturn := PackedStringArray()
+static func find_scripts_of_class( scripts :PackedStringArray, klass ) -> PackedStringArray:
+	var to_return := PackedStringArray()
 
 	for script in scripts:
 		if script.get_extension() != "gd":
@@ -44,9 +40,45 @@ static func find_scripts_of_class( scripts: PackedStringArray, klass ):
 
 		var object = load(script).new()
 		if is_instance_of(object, klass):
-			scriptsToReturn.append(script)
+			to_return.append(script)
 
 		if not object is RefCounted:
 			object.free()
 
-	return scriptsToReturn
+	return to_return
+	
+	
+#this includes subclasses
+static func find_resources_of_class( files :PackedStringArray, klass ) -> PackedStringArray:
+	var to_return := PackedStringArray()
+	
+	for file in files:
+		if file.get_extension() != "tres":
+			continue
+			
+		var resource = load(file)
+		if is_instance_of(resource, klass):
+			to_return.append(file)
+
+	return to_return
+
+
+static func find_resources_of_class_name(
+		files :PackedStringArray, klass_name :String ) -> PackedStringArray:
+
+	var to_return := PackedStringArray()
+	const SCRIPT_CLASS_KEY := ' script_class="'
+
+	for file_path in files:
+		if file_path.get_extension() != "tres":
+			continue
+		
+		var file = FileAccess.open(file_path, FileAccess.READ)
+		var split_first_line = file.get_line().split(SCRIPT_CLASS_KEY)
+		if split_first_line.size() <= 1:
+			continue
+			
+		if split_first_line[1].begins_with(klass_name+'"'):
+			to_return.append(file_path)
+			
+	return to_return
